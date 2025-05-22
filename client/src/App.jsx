1decefb727182162629aa49f4c364f5ce818d4cf
Sudharsan V <176049812+Sudharsanv151@users.noreply.github.com>
2025-05-22 22:42:53 +0530

    Added routers, components,

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,22 @@ import './App.css'
 import Home from './pages/Home'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <div className='App'>
-      <Header/>
-      <Home/>
-      <Footer/>
+      <Router>
+        <div>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/search' element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
+      <Footer />
     </div>
   )
 }
--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -2,8 +2,11 @@ const productModel=require('../models/productModel');
 
 // - /api/v1/products
 exports.getProducts= async (req,res,next)=>{
-    
-    const products=await productModel.find({});
+    const query =req.query.keyword? { name: { 
+        $regex: req.query.keyword,
+        $options: 'i'
+    }} : {}
+    const products=await productModel.find(query);
     
     res.json({
         success:true,
